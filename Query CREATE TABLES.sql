3ef7a73b121d8a7b224f5135a2bac749e34c14d5
Halunachervoniak <chbhaluna@gmail.com>
2018-07-17 01:01:54 +0300

    Create tables ProductCategories, ProductSubCategories, ReturnProduct

--- Query CREATE TABLES.sql
+++ Query CREATE TABLES.sql
@@ -1,28 +1,54 @@
 
-CREATE TABLE ProductSubCategories (
-  SubCategoryID int NOT NULL IDENTITY(1,1),
-  CategoryID int NOT NULL,
-  SubCategoryName nvarchar(20) NOT NULL,
-  ProdSubCatDescription nvarchar(300) NOT NULL,
-  Active bit NOT NULL,
-  CONSTRAINT PK_ProductSubCategories PRIMARY KEY CLUSTERED (SubCategoryID) );
-  GO
 
-CREATE TABLE ReturnProducts (
-  ReturnProductId int NOT NULL IDENTITY(1,1),
-  ProductId int NOT NULL,
-  Quantity int NOT NULL,
-  OrderID int NOT NULL,
-  ReturnType nvarchar(20) NULL,
-  ReturnProdDescription nvarchar(300) NULL,
-  DateMonth date NOT NULL,
-  BusinessUnitID int NOT NULL,
-  CONSTRAINT PK_ReturnProducts PRIMARY KEY CLUSTERED (ReturnProductId) );
-  GO
+CREATE TABLE [dbo].[ProductCategories](
+	[CategoryID] [INT] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [NVARCHAR](50) NOT NULL,
+	[Active] [BIT] NOT NULL,
+ CONSTRAINT [PK_ProductCategories] PRIMARY KEY CLUSTERED 
+(
+	[CategoryID]
+)
+
+GO
+
+CREATE TABLE [dbo].[ProductSubCategories](
+	[CategoryID] [INT] NOT NULL,
+	[CategoryName] [NVARCHAR](50) NOT NULL,
+	[SubCategoryID] [INT] IDENTITY(1,1) NOT NULL,
+	[SubCategoryName] [NVARCHAR](50) NOT NULL,
+ CONSTRAINT [PK_ProductSubCategories] PRIMARY KEY CLUSTERED 
+(
+	[SubCategoryID])
+
+GO
+
+CREATE TABLE [dbo].[ReturnProducts](
+	[ReturnProductID] [INT] IDENTITY(1,1) NOT NULL,
+	[ProductID] [INT] NULL,
+	[Quantity] [INT] NULL,
+	[OrderID] [INT] NULL,
+	[ReturnType] [NVARCHAR](20) NULL,
+	[DateMonth] [DATE] NULL,
+	[BusinessUnitID] [INT] NULL,
+ CONSTRAINT [PK_ReturnProducts] PRIMARY KEY CLUSTERED 
+(
+	[ReturnProductID] 
+)
+GO
+
+ALTER TABLE [dbo].[ReturnProducts]  WITH CHECK ADD FOREIGN KEY([OrderID])
+REFERENCES [dbo].[Orders] ([OrderID])
+GO
+
+ALTER TABLE [dbo].[ReturnProducts]  WITH CHECK ADD  CONSTRAINT [CHK_Quantity] CHECK  (([Quantity]>(0)))
+GO
+
   -- =========================================================================
 
   DROP TABLE ProductSubCategories
   GO
   DROP TABLE ReturnProducts
+  GO
+  DROP TABLE ProductCategories
   GO
   -- ==========================================================================
